d6c1acd0814114b965926940ca0e867f39d2ec11
javierxnora <javieratnora@gmail.com>
2020-11-14 17:05:34 -0300

    fuck

--- app/Controllers/Http/EventsController.ts
+++ app/Controllers/Http/EventsController.ts
@@ -1,14 +1,15 @@
-//import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Event from 'App/Models/Event'
 
 export default class EventsController {
-  public async index () {
+  public async index (ctx: HttpContextContract) {
     const eventos = await Event
                           .query()
                           .preload('status')
                           .preload('type')    
                           .preload('evaluation', (query) =>{ query.preload('subject', (query) => {query.preload('grade')})})   
-                          .preload('subject', (query) => query.preload('grade'))                              
+                          .preload('subject', (query) => query.preload('grade')) 
+                          .where('events.date', ctx.request.get().eventDate)                             
     return eventos;
     }
 
